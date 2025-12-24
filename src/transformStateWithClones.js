a4150f4e69e33b3c3fb8967e84654bb37dd9a301
arsenkostych-gif <arsenkostych@gmail.com>
2025-12-24 14:08:26 +0100

    виконав завдання

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,40 @@
-'use strict';
-
-/**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
- */
 function transformStateWithClones(state, actions) {
-  // write code here
-}
+  const history = [];
+  // Create a working copy of the initial state
+  let currentState = { ...state };
 
-module.exports = transformStateWithClones;
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        // Reset to an empty object
+        currentState = {};
+        break;
+
+      case 'addProperties':
+        // Merge current state with extraData using spread syntax
+        currentState = {
+          ...currentState,
+          ...action.extraData
+        };
+        break;
+
+      case 'removeProperties':
+        // Create a new copy to modify
+        const nextState = { ...currentState };
+        action.keysToRemove.forEach(key => {
+          delete nextState[key];
+        });
+        currentState = nextState;
+        break;
+
+      default:
+        // If an unknown action type is passed, we keep the state as is
+        break;
+    }
+
+    // Push a new clone of the current state into our history array
+    history.push({ ...currentState });
+  }
+
+  return history;
+}
